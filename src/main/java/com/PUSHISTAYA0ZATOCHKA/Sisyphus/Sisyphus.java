3e29e44d535bc696f353b77e7a49d620efc308f1
Pablo <155914874+PUSHISTAYA0ZATOCHKA@users.noreply.github.com>
2024-10-10 19:07:51 +0300

    finale f work

---
+++ src/main/java/com/PUSHISTAYA0ZATOCHKA/Sisyphus/Sisyphus.java
@@ -0,0 +1,19 @@
+package com.PUSHISTAYA0ZATOCHKA.Sisyphus;
+
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+
+public class Sisyphus extends GameApplication {
+
+	@Override
+	protected void initSettings(GameSettings settings) {
+		settings.setTitle("S");
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		launch(args);
+	}
+
+}
