db5ef08ee29bfaabf680c212f736dadd9c0caaa9
Good1Cheese <work.cheese@erine.email>
2024-10-11 22:30:12 +0300

    :sparkles: Maps

---
+++ maps/Woodland_ground.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Woodland_ground" tilewidth="64" tileheight="64" tilecount="3" columns="3">
+ <image source="tiled_examples/The Mana World 2/Woodland_ground.png" width="192" height="96"/>
+</tileset>
