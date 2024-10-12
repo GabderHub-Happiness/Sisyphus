92c2f7a7cea15bd0362ec9ddf42da033a54f38ea
Good1Cheese <work.cheese@erine.email>
2024-10-12 23:46:40 +0300

    :sparkles: detect mouse click

--- app/src/main/java/org/gabhub/App.java
+++ app/src/main/java/org/gabhub/App.java
@@ -1,10 +1,27 @@
 package org.gabhub;
 
 import de.gurkenlabs.litiengine.Game;
+//import de.gurkenlabs.
+import de.gurkenlabs.litiengine.IUpdateable;
+import de.gurkenlabs.litiengine.input.IMouse;
+import de.gurkenlabs.litiengine.input.Input;
+import de.gurkenlabs.litiengine.input.Mouse;
 
 public class App {
     public static void main(String[] args) {
+        Game.info().setName("Sisyphys");
+        Game.info().setSubTitle("");
+
         Game.init(args);
+
+        // Detecting left click
+        Game.loop().attach(() -> {
+            if (Input.mouse().isPressed() && Input.mouse().isLeftButtonPressed()) {
+                System.out.println("LEFT");
+            }
+        });
+
+
         Game.start();
     }
 }
